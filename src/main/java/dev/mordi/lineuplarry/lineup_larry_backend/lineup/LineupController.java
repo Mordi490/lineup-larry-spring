39b65e0b02d6ff14221b07db0d9a9388e83f2c78
Mordi490 <jananton007@hotmail.com>
2024-06-26 14:34:00 +0200

    changed status code on creation to 201

--- src/main/java/dev/mordi/lineuplarry/lineup_larry_backend/lineup/LineupController.java
+++ src/main/java/dev/mordi/lineuplarry/lineup_larry_backend/lineup/LineupController.java
@@ -1,14 +1,8 @@
 package dev.mordi.lineuplarry.lineup_larry_backend.lineup;
 
 import org.jooq.DSLContext;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,6 +35,7 @@ public class LineupController {
     }
 
     @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
     public Lineup createLineup(@RequestBody Lineup lineup) {
         System.out.println("Received lineup (controller):" + lineup);
         return repository.createLineup(lineup);
