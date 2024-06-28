382ea540be8c509d8c50f7c826fad2bc3b6dc039
Mordi490 <jananton007@hotmail.com>
2024-06-28 17:10:17 +0200

    declutter main file

--- src/main/java/dev/mordi/lineuplarry/lineup_larry_backend/Application.java
+++ src/main/java/dev/mordi/lineuplarry/lineup_larry_backend/Application.java
@@ -1,13 +1,7 @@
 package dev.mordi.lineuplarry.lineup_larry_backend;
 
-import dev.mordi.lineuplarry.lineup_larry_backend.lineup.Lineup;
-import dev.mordi.lineuplarry.lineup_larry_backend.lineup.LineupRepository;
-import dev.mordi.lineuplarry.lineup_larry_backend.user.User;
-import dev.mordi.lineuplarry.lineup_larry_backend.user.UserRepository;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -16,23 +10,5 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    ApplicationRunner applicationRunner(UserRepository userRepository,
-                                        LineupRepository lineupRepository) {
-        return args -> {
-            User userOne = new User(null, "userOne");
-            User userTwo = new User(null, "userOne");
-            userRepository.createUser(userOne);
-            userRepository.createUser(userTwo);
-
-            Lineup lineupOne = new Lineup(null, "lineup one title", "lineup body", 1L); // same
-            Lineup lineupTwo = new Lineup(2L, "lineup two title", "lineup body", 2L);
-            Lineup lineupThree = new Lineup(3L, "lineup three title", "lineup body", 2L);
-            lineupRepository.createLineup(lineupOne);
-            lineupRepository.createLineup(lineupTwo);
-            lineupRepository.createLineup(lineupThree);
-        };
     }
 }
