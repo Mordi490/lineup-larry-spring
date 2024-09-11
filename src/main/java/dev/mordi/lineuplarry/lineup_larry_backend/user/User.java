525beea59c133e526179918f35659f74509407a4
Mordi490 <jananton007@hotmail.com>
2024-09-11 17:37:34 +0200

    updated size on username

--- src/main/java/dev/mordi/lineuplarry/lineup_larry_backend/user/User.java
+++ src/main/java/dev/mordi/lineuplarry/lineup_larry_backend/user/User.java
@@ -8,7 +8,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record User(@Nullable Long id,
-                   @Size(min = 1, max = 34, message = "Username has to be between {min} and {max} characters")
+                   // NB! this will most likely just be the discord username, which has min 2 and max 32
+                   @Size(min = 2, max = 32, message = "Username has to be between {min} and {max} characters")
                    @NotNull(message = "username cannot be null")
                    @NotEmpty(message = "username cannot be empty")
                    @NotBlank(message = "username cannot be blank")
