177da6863ce7f00cd942d01c578b922f0cce978e
Mordi490 <jananton007@hotmail.com>
2024-10-30 17:10:34 +0100

    createdAt & updatedAt fields and trigger for updatedAt

--- src/main/resources/db/migration/V1__init-schema.sql
+++ src/main/resources/db/migration/V1__init-schema.sql
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS
         id bigserial not null,
         username text not null,
         primary key (id)
-        -- TODO: add create_at & last_updated_at
+        -- TODO: add created_at
     );
 
 CREATE TABLE IF NOT EXISTS
@@ -56,18 +56,33 @@ CREATE TABLE IF NOT EXISTS
         title text not null,
         body text not null,
         user_id bigint REFERENCES users (id) ON DELETE CASCADE not null,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+        updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
         primary key (id)
-        -- TODO: add create_at & last_updated_at
-        -- TODO: add patch, patches will eventually include map changes
     );
+
+    -- trigger to update the "updated_at" field
+    CREATE OR REPLACE FUNCTION update_last_updated_at()
+    RETURNS TRIGGER AS $$
+    BEGIN
+        NEW.updated_at = NOW();
+        RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;
+
+    CREATE TRIGGER set_updated_at
+    BEFORE UPDATE ON lineup
+    FOR EACH ROW
+    EXECUTE FUNCTION update_last_updated_at();
 
  CREATE TABLE IF NOT EXISTS
     likes (
         user_id bigint REFERENCES users (id) ON DELETE CASCADE,
         lineup_id bigint REFERENCES lineup (id) ON DELETE CASCADE,
-        liked_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
         PRIMARY KEY (user_id, lineup_id)
     );
+
 
     -- TODO: groups
 
