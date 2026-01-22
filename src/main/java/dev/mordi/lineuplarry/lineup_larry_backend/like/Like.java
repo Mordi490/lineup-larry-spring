43d6d8ec6d91f21f863c446d51522bce79f72783
Mordi490 <jananton007@hotmail.com>
2026-01-22 20:42:54 +0100

    removed all files, excpect mise and git

--- src/main/java/dev/mordi/lineuplarry/lineup_larry_backend/like/Like.java
+++
@@ -1,6 +0,0 @@
-package dev.mordi.lineuplarry.lineup_larry_backend.like;
-
-import jakarta.annotation.Nullable;
-import java.time.OffsetDateTime;
-
-public record Like(Long userId, Long lineupId, @Nullable OffsetDateTime createdAt) {}
--- src/main/java/dev/mordi/lineuplarry/lineup_larry_backend/lineup/LineupIdTitleDTO.java
+++
@@ -1,3 +0,0 @@
-package dev.mordi.lineuplarry.lineup_larry_backend.lineup;
-
-public record LineupIdTitleDTO(Long id, String title) {}
