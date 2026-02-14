a52bcddab36aaf99b077de28d0a1e32ca7b53c20
Mordi490 <jananton007@hotmail.com>
2026-02-14 23:20:05 +0100

    return problemDetails from controllers

--- src/main/java/dev/mordi/lineuplarry/lineup_larry_backend/lineup/LineupController.java
+++ src/main/java/dev/mordi/lineuplarry/lineup_larry_backend/lineup/LineupController.java
@@ -1,5 +1,6 @@
 package dev.mordi.lineuplarry.lineup_larry_backend.lineup;
 
+import dev.mordi.lineuplarry.lineup_larry_backend.lineup.exceptions.InvalidLineupException;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Size;
 import java.util.List;
@@ -14,63 +15,79 @@ import org.springframework.web.bind.annotation.*;
 @Validated
 public class LineupController {
 
-  LineupService lineupService;
+    LineupService lineupService;
 
-  LineupController(LineupService lineupService) {
-    this.lineupService = lineupService;
-  }
+    LineupController(LineupService lineupService) {
+        this.lineupService = lineupService;
+    }
 
-  @GetMapping("/ping")
-  public String ping() {
-    return "pong";
-  }
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
 
-  @GetMapping
-  public ResponseEntity<List<LineupWithAuthorDTO>> getLineups(
-      @RequestParam(required = false)
-          @Size(min = 3, max = 40, message = "Title must be between {min} and {max} characters")
-          String title,
-      // maybe consider custom validations
-      @RequestParam(required = false) String agent,
-      @RequestParam(required = false) String map,
-      @RequestParam(required = false, defaultValue = "20") Long pageSize,
-      @RequestParam(required = false) Optional<Long> lastValue) {
-    List<LineupWithAuthorDTO> lineups =
-        lineupService.getLineup(title, agent, map, pageSize, lastValue.orElse(null));
-    return new ResponseEntity<>(lineups, HttpStatus.OK);
-  }
+    @GetMapping
+    public ResponseEntity<List<LineupWithAuthorDTO>> getLineups(
+        @RequestParam(required = false) @Size(
+            min = 3,
+            max = 40,
+            message = "Title must be between {min} and {max} characters"
+        ) String title,
+        // maybe consider custom validations
+        @RequestParam(required = false) String agent,
+        @RequestParam(required = false) String map,
+        @RequestParam(required = false, defaultValue = "20") Long pageSize,
+        @RequestParam(required = false) Optional<Long> lastValue
+    ) {
+        List<LineupWithAuthorDTO> lineups = lineupService.getLineup(
+            title,
+            agent,
+            map,
+            pageSize,
+            lastValue.orElse(null)
+        );
+        return new ResponseEntity<>(lineups, HttpStatus.OK);
+    }
 
-  @GetMapping("/{id}")
-  public ResponseEntity<LineupWithAuthorDTO> getById(@PathVariable Long id) {
-    Optional<LineupWithAuthorDTO> lineup = lineupService.getById(id);
-    return lineup.map(ResponseEntity::ok).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-  }
+    @GetMapping("/{id}")
+    public ResponseEntity<LineupWithAuthorDTO> getById(@PathVariable Long id) {
+        LineupWithAuthorDTO lineup = lineupService
+            .getById(id)
+            .orElseThrow(() ->
+                new InvalidLineupException.NoSuchLineupException(id)
+            );
+        return ResponseEntity.ok(lineup);
+    }
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public Lineup createLineup(@Valid @RequestBody Lineup lineup) {
-    return lineupService.createLineup(lineup);
-  }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Lineup createLineup(@Valid @RequestBody Lineup lineup) {
+        return lineupService.createLineup(lineup);
+    }
 
-  @PutMapping("/{id}")
-  public void updateLineup(@PathVariable Long id, @Valid @RequestBody Lineup lineup) {
-    lineupService.updateLineup(id, lineup);
-  }
+    @PutMapping("/{id}")
+    public void updateLineup(
+        @PathVariable Long id,
+        @Valid @RequestBody Lineup lineup
+    ) {
+        lineupService.updateLineup(id, lineup);
+    }
 
-  @DeleteMapping("/{id}")
-  @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void deleteLineup(@PathVariable Long id) {
-    lineupService.deleteLineup(id);
-  }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteLineup(@PathVariable Long id) {
+        lineupService.deleteLineup(id);
+    }
 
-  @GetMapping("/user/{id}")
-  public ResponseEntity<List<LineupWithAuthorDTO>> getAllLineupsFromUser(
-      @PathVariable Long id,
-      @RequestParam(required = false, defaultValue = "20") Long pageSize,
-      @RequestParam(required = false) Optional<Long> lastValue) {
-    Optional<List<LineupWithAuthorDTO>> lineups =
-        lineupService.getAllLineupsFromUserId(id, pageSize, lastValue.orElse(null));
-
-    return lineups.map(ResponseEntity::ok).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-  }
+    @GetMapping("/user/{id}")
+    public ResponseEntity<List<LineupWithAuthorDTO>> getAllLineupsFromUser(
+        @PathVariable Long id,
+        @RequestParam(required = false, defaultValue = "20") Long pageSize,
+        @RequestParam(required = false) Optional<Long> lastValue
+    ) {
+        List<LineupWithAuthorDTO> lineups = lineupService
+            .getAllLineupsFromUserId(id, pageSize, lastValue.orElse(null))
+            .orElseThrow(() -> new InvalidLineupException.NoUserException(id));
+        return ResponseEntity.ok(lineups);
+    }
 }
--- src/main/java/dev/mordi/lineuplarry/lineup_larry_backend/user/UserController.java
+++ src/main/java/dev/mordi/lineuplarry/lineup_larry_backend/user/UserController.java
@@ -1,8 +1,8 @@
 package dev.mordi.lineuplarry.lineup_larry_backend.user;
 
+import dev.mordi.lineuplarry.lineup_larry_backend.user.exceptions.InvalidUserException;
 import jakarta.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,49 +11,58 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 public class UserController {
 
-  private final UserService userService;
+    private final UserService userService;
 
-  UserController(UserService userService) {
-    this.userService = userService;
-  }
+    UserController(UserService userService) {
+        this.userService = userService;
+    }
 
-  @GetMapping("ping")
-  public String ping() {
-    return "pong";
-  }
+    @GetMapping("ping")
+    public String ping() {
+        return "pong";
+    }
 
-  @GetMapping()
-  public List<User> getAllUsers() {
-    return userService.getAll();
-  }
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAll();
+    }
 
-  @GetMapping("/{id}")
-  public ResponseEntity<User> getUserById(@PathVariable Long id) {
-    Optional<User> user = userService.getById(id);
-    return user.map(ResponseEntity::ok).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-  }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService
+            .getById(id)
+            .orElseThrow(() ->
+                new InvalidUserException.UserNotFoundException(id)
+            );
+        return ResponseEntity.ok(user);
+    }
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public User createUser(@Valid @RequestBody User user) {
-    return userService.createUser(user);
-  }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@Valid @RequestBody User user) {
+        return userService.createUser(user);
+    }
 
-  @PutMapping("/{id}")
-  public void updateUser(@PathVariable Long id, @Valid @RequestBody User user) {
-    userService.updateUser(id, user);
-  }
+    @PutMapping("/{id}")
+    public void updateUser(
+        @PathVariable Long id,
+        @Valid @RequestBody User user
+    ) {
+        userService.updateUser(id, user);
+    }
 
-  @DeleteMapping("/{id}")
-  @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void deleteUser(@PathVariable Long id) {
-    userService.deleteUser(id);
-  }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
 
-  @GetMapping("/summary/{id}")
-  public ResponseEntity<UserSummaryDTO> getUserSummary(@PathVariable Long id) {
-    UserSummaryDTO ar = userService.getUserSummary(id);
-    return new ResponseEntity<>(ar, HttpStatus.OK);
-  }
-  // TODO: after auth:
+    @GetMapping("/summary/{id}")
+    public ResponseEntity<UserSummaryDTO> getUserSummary(
+        @PathVariable Long id
+    ) {
+        UserSummaryDTO ar = userService.getUserSummary(id);
+        return new ResponseEntity<>(ar, HttpStatus.OK);
+    }
+    // TODO: after auth:
 }
